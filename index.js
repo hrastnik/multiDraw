f0150539e53e8a8803e7af4cd37f64b079af752d
mc-jesus <hros_the_boss@hotmail.com>
2015-09-14 14:30:30 +0200

    Fixed some bugs

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ http = require('http').Server(app);
 var io = require("socket.io")(http);
 var fs = require('fs');
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function(req, res) {
     fs.readFile('main.html', function(err, data) {
         res.send(data.toString());
@@ -10,9 +12,8 @@ app.get('/', function(req, res) {
 });
 
 io.on("connection", function(s) {
-    s.on('d', function(coords) {io.emit('d', coords);});
-    s.on('m', function(coords) {io.emit('m', coords);});
-    s.on('u', function(coords) {io.emit('u', coords);});    
+    s.on('m', function(msg) {s.broadcast.emit('m', msg);});
+    s.on('s', function(line_id) {s.broadcast.emit('s', line_id);});
 });
 
-http.listen(8000);
+http.listen(app.get('port'));
